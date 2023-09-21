b14a6b2345fe39ceed82815bc69f630a80d5eee0
skudryavtsev01 <48890788+skudryavtsev01@users.noreply.github.com>
2023-09-21 17:49:02 +0300

    Добавьте файлы проекта.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyTest.Data;
+using MyTest.Models;
+using System.Collections.Generic;
+
+namespace MyTest.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        public CategoryController(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+        
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = db.Category;
+            return View(objList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            db.Category.Add(obj);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyTest.Models;
+
+namespace MyTest.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Category> Category {  get; set; }
+
+    }
+}

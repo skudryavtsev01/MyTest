733e067a3e4fc223d55f1738888cf5aab7bfdddb
skudryavtsev01 <48890788+skudryavtsev01@users.noreply.github.com>
2023-10-04 22:06:31 +0300

    2023-10-04 2206

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MyTest.Data;
+using MyTest.Models;
+using MyTest.Models.ViewModel;
+using System.Collections.Generic;
+
+namespace MyTest.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        public ProductController(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+        
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objList = db.Product;
+
+            foreach (var obj in objList) 
+            {
+                obj.Category = db.Category.FirstOrDefault(u => u.Id == obj.CategoryId);
+            }
+
+            return View(objList);
+        }
+
+        //GET - UPSERT
+        public IActionResult Upsert(int? id)
+        {
+
+            //IEnumerable<SelectListItem> CategoryDropDown = db.Category.Select(i => new SelectListItem()
+            //{
+            //    Text = i.Name,
+            //    Value = i.Id.ToString()
+            //});
+
+            //ViewBag.CategoryDropDown = CategoryDropDown;
+
+            //Product product = new Product();
+
+            ProductVM productVM = new ProductVM()
+            {
+                Product = new Product(),
+                CategorySelectList = db.Category.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                })
+            };
+
+            if (id == null)
+            {
+                //this is for create
+                return View(productVM);
+            }
+            else 
+            {
+                //this is for edit
+                productVM.Product = db.Product.Find(id);
+                if (productVM.Product == null)
+                {
+                    return NotFound();
+                }
+                return View(productVM);
+            }
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Product obj)
+        {
+            if(ModelState.IsValid) 
+            {
+                db.Product.Add(obj);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = db.Product.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = db.Product.Find(id);
+            if (ModelState.IsValid)
+            {
+                db.Product.Remove(obj);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+}
---
+++ Models/ViewModel/ProductVM.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MyTest.Models.ViewModel
+{
+    public class ProductVM
+    {
+        public Product Product { get; set; }
+        public IEnumerable<SelectListItem> CategorySelectList { get; set; }
+    }
+}
